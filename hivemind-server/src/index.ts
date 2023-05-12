e38eb61bc8801534c0e66f7d45650db2d0df567b
mozammalrahat <hossain.rahat33@gmail.com>
2023-05-13 03:25:32 +0600

    Setup -->> GraphQl, ApolloServer, Express, TypeGraphQL Added -->> mikroORM TypeGraphQL Crud Operations

--- hivemind-server/src/index.ts
+++ hivemind-server/src/index.ts
@@ -1,8 +1,12 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
 import mikroOrmConfig from "./mikro-orm.config";
-// import { PostgreSqlDriver } from "@mikro-orm/postgresql";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { PostResolver } from "./resolvers/post";
+import "reflect-metadata";
+
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
   await orm.getMigrator().up();
@@ -10,13 +14,27 @@ const main = async () => {
   const generator = orm.getSchemaGenerator();
   await generator.updateSchema();
 
+  const app = express();
+
   //   const post = orm.em.fork().create(Post, {
   //     title: "1st Post",
   //   } as RequiredEntityData<Post>);
   //   await orm.em.persistAndFlush(post);
 
-  const posts = await orm.em.find(Post, {});
-  console.log(posts);
+  // const posts = await orm.em.find(Post, {});
+  // console.log(posts);
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }), // can also add req and res here.
+  });
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
+  app.listen(3000, () => {
+    console.log("Hivemind server running on port 3000");
+  });
 };
 
 main();
---
+++ hivemind-server/src/resolvers/post.ts
@@ -0,0 +1,64 @@
+import { Post } from "../entities/Post";
+import { MyContext } from "src/types";
+import { Resolver, Query, Ctx, Arg, Int, Mutation } from "type-graphql";
+
+@Resolver()
+export class PostResolver {
+  @Query(() => [Post])
+  async posts(@Ctx() { em }: MyContext): Promise<Post[] | null> {
+    return await em.find(Post, {});
+  }
+
+  @Query(() => Post, { nullable: true })
+  async post(
+    @Arg("id", () => Int) id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    return await em.findOne(Post, { id });
+  }
+
+  @Mutation(() => Post)
+  async createPost(
+    @Arg("title") title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    const post = em.create(Post, {
+      title,
+    });
+    await em.persistAndFlush(post);
+    return post;
+  }
+
+  @Mutation(() => Post, { nullable: true })
+  async updatePost(
+    @Arg("id") id: number,
+    @Arg("title", () => String, { nullable: true }) title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    const post = await em.findOne(Post, { id });
+    if (!post) {
+      return null;
+    }
+    if (typeof title !== "undefined") {
+      post.title = title;
+      await em.persistAndFlush(post);
+    }
+    return post;
+  }
+
+  @Mutation(() => Boolean)
+  async deletePost(
+    @Arg("id") id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Boolean> {
+    try {
+      await em.nativeDelete(Post, {
+        id,
+      });
+    } catch {
+      return false;
+    }
+
+    return true;
+  }
+}
